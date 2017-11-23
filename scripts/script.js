88ed7fa49bf33e78776030b2a7d5a9614389e6d3
konnor <konnor5456@gmail.com>
2017-11-23 04:15:55 -0500

    ccreated a basic grid using float:left, also created the intial files and directories

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    for (var row = 0; row < 16; row++){
+        for (var column = 0; column < 16; column++){
+            $(".table").append("<div class=grid> </div>");
+            
+            // console.log(row + " " + column);
+        }
+    }
+});
