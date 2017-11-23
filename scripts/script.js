d3c3f39fb562ec8cb9f24a9dadf65958274cb31e
ParamagicDev <konnor5456@gmail.com>
2017-11-23 04:45:02 -0500

    added a mouseenter function. Changes the background to black when the mouse enters

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,17 @@
 $(document).ready(function(){
     for (var row = 0; row < 16; row++){
         for (var column = 0; column < 16; column++){
-            $(".table").append("<div class=grid> </div>");
+        
+            $(".table").append("<div class=grid id=" + row + "," + column + "> </div>");
             
             // console.log(row + " " + column);
         }
     }
+    // creates the grid
+
+    $(".grid").on("mouseenter", function(){
+        $(this).css({
+            "background-color" : "black"
+        })
+    });
 });
