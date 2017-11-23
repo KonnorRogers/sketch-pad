6b3b1d07c1f9b24d4ef4a05ccb93b1cf748c4a09
ParamagicDev <konnor5456@gmail.com>
2017-11-23 16:28:05 -0500

    troubleshooted the grid, now displays properly

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,24 @@
-$(document).ready(function(){
-    for (var row = 0; row < 16; row++){
-        for (var column = 0; column < 16; column++){
-        
-            $(".table").append("<div class=grid id=" + row + "," + column + "> </div>");
-            
+function createGrid(x){
+    for (var row = 0; row < x; row++){
+        for (var column = 0; column < x; column++){
+            $("#container").append("<div class='grid'> </div>");
             // console.log(row + " " + column);
-        }
-    }
-    // creates the grid
+        };
+    };
+    $(".grid").width(500 / x);
+    $(".grid").height(500 / x);
+};  // creates the grid
+
+function clearGrid(){
+    $(".grid").remove();
+}
+
+$(document).ready(function(){
+    createGrid(16);    
 
     $(".grid").on("mouseenter", function(){
         $(this).css({
             "background-color" : "black"
-        })
+        });
     });
 });
